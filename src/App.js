4e1df414a259df27be4bb9eb0b4ddbec8a62ada4
Markus Scheidgen <markus.scheidgen@gmail.com>
2020-05-31 13:24:12 +0200

    Separated grid, polygons, and lines.

--- src/App.js
+++ src/App.js
@@ -3,39 +3,117 @@ import "./styles.css";
 
 // This is based on https://www.redblobgames.com/grids/hexagons/
 
-const w = Math.sqrt(3) * 50;
-const h = 2 * 50;
+class OffsetCoords {
+  constructor(size) {
+    this.size = size;
+    this.w = Math.sqrt(3) * this.size;
+    this.h = 2 * this.size;
+  }
 
-function Polygon({ x, y }) {
-  const offset =
-    y % 2 == 0 ? [x * w, y * h * 0.75] : [(x + 0.5) * w, y * h * 0.75];
+  center(x, y) {
+    return y % 2 == 0
+      ? [x * this.w, y * this.h * 0.75]
+      : [(x + 0.5) * this.w, y * this.h * 0.75];
+  }
+
+  centerRelativePoints() {
+    return [
+      [-0.5 * this.w, -0.25 * this.h],
+      [-0.5 * this.w, 0.25 * this.h],
+      [0, 0.5 * this.h],
+      [0.5 * this.w, 0.25 * this.h],
+      [0.5 * this.w, -0.25 * this.h],
+      [0, -0.5 * this.h]
+    ];
+  }
+}
+
+function Polygon({ x, y, offsetCoords }) {
+  const center = offsetCoords.center(x, y);
   return (
-    <g transform={`translate(${offset[0] + 150}, ${offset[1] + 150})`}>
+    <g transform={`translate(${center[0]}, ${center[1]})`}>
+      <polygon
+        points={offsetCoords
+          .centerRelativePoints()
+          .map(([x, y]) => `${x},${y}`)
+          .join(" ")}
+        fill="grey"
+      />
       <circle r="5" x="0" y="0" fill="red" />
+    </g>
+  );
+}
 
-      <line x1="0" y1={h * 0.5} x2={w * 0.5} y2={h * 0.25} stroke="black" />
-      <line x1="0" y1={h * 0.5} x2={-w * 0.5} y2={h * 0.25} stroke="black" />
-      <line x1="0" y1={h * 0.5} x2="0" y2={h} stroke="black" />
+function Line({ x, y, sides, offsetCoords }) {
+  const center = offsetCoords.center(x, y);
+  const points = offsetCoords.centerRelativePoints();
+  return (
+    <g transform={`translate(${center[0]}, ${center[1]})`}>
+      {sides.map(side => (
+        <line
+          x1={points[side][0]}
+          y1={points[side][1]}
+          x2={points[(side + 1) % 6][0]}
+          y2={points[(side + 1) % 6][1]}
+          stroke="black"
+        />
+      ))}
+    </g>
+  );
+}
+
+function Grid({ w, h, offsetCoords }) {
+  const sides = (x, y) => {
+    const result = [0, 1, 2];
+    if (y + 1 === w) {
+      result.push(3);
+    }
+    return result;
+  };
+  return (
+    <g>
+      {[...Array(w).keys()].map(x =>
+        [...Array(h).keys()].map(y => (
+          <React.Fragment>
+            <Polygon
+              key={`poly${x},${y}`}
+              x={x}
+              y={y}
+              offsetCoords={offsetCoords}
+            />
+            <Line
+              key={`line${x},${y}`}
+              x={x}
+              y={y}
+              offsetCoords={offsetCoords}
+              sides={sides(x, y)}
+            />
+          </React.Fragment>
+        ))
+      )}
     </g>
   );
 }
 
 export default function App() {
+  const offsetCoords = new OffsetCoords(50);
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <svg with="500" height="500">
-        <Polygon x={0} y={0} />
-        <Polygon x={1} y={0} />
-        <Polygon x={0} y={1} />
-        <Polygon x={1} y={1} />
-        <Polygon x={0} y={2} />
-        <Polygon x={1} y={2} />
-        <Polygon x={0} y={3} />
-        <Polygon x={1} y={3} />
-        <Polygon x={0} y={4} />
-        <Polygon x={1} y={4} />
+      <svg
+        style={{
+          position: "fixed",
+          left: "0",
+          top: "0",
+          width: "100%",
+          height: "100%"
+        }}
+      >
+        <g
+          transform={`translate(${offsetCoords.w * 0.5}, ${offsetCoords.h *
+            0.5})`}
+        >
+          <Grid w={10} h={10} offsetCoords={offsetCoords} />
+        </g>
       </svg>
     </div>
   );
